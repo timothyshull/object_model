bc6ce83dc5da98a1eac67bee9624d8459d093c7f
Tim Shull <timothyshull@gmail.com>
2017-06-18 20:37:49 -0700

    Add script to read classes and pull descriptions

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ talk. The majority of the examples and the general outline for the
 project is based on Stanley Lippman's "Inside the C++ Object Model",
 with additional resources drawn from cppreference.com and Anthony
 Williams' "C++ Concurrency in Action". The main purpose of this
-project is to use Hopper (https://www.hopperapp.com/) and various
-features of clang to expose some of the underlying functionality that
-facilitates the object model in C++.
+project is to use Hopper (https://www.hopperapp.com/) and clang/clang
+tooling to expose the inner workings of key features (OO & polymorphism,
+templates, exceptions, etc.) in C++.
 
 The profiling sub-project is dependent on Google benchmark (https://github.com/google/benchmark).
 
@@ -16,4 +16,3 @@ Run `./build.sh` in the root directory of the project for automated file generat
 project setup options.
 
 This project is currently under construction.
-
--- get_all_ast_classes.py
+++ get_all_ast_classes.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+# NOTE: this is a quick and dirty way to read all of the AST classes used in the project files and grab the description
+# of the class from the clang doxygen docs
+
 import os
 import glob
 import re
+import requests
+from bs4 import BeautifulSoup
+import lxml
 
 class_filename = 'all_ast_classes.txt'
+clang_url_format_str = 'http://clang.llvm.org/doxygen/classclang_1_1{0}.html'
 
 
 def get_all_ast_files(root_dir):
@@ -19,18 +26,48 @@ def get_classes_in_file(filename):
     return re.findall('\|-([A-Z]\w*)\s', all_text) + re.findall('`-([A-Z]\w*)\s', all_text)
 
 
-def write_class_file(root_dir, all_classes):
+def prettify_class_description(class_name, description):
+    out_str = class_name + '\nDescription:\n'
+    for l in description.splitlines():
+        out_str += '    ' + l + '\n'
+    return out_str + '\n\n'
+
+
+def get_class_description(class_name):
+    request = requests.get(clang_url_format_str.format(class_name))
+
+    if request.status_code == 200:
+        parsed_contents = BeautifulSoup(request.content, 'lxml')
+        return prettify_class_description(
+            class_name,
+            parsed_contents.find('h2', string='Detailed Description').find_next_sibling().get_text()
+        )
+    else:
+        return class_name + '\n\n'
+
+
+def write_class_file(root_dir, all_classes, get_description=False):
     global class_filename
+
+    out_str = ''
+
+    if get_description:
+        for c in all_classes:
+            out_str += get_class_description(c)
+    else:
+        out_str = '\n'.join(all_classes) + '\n'
+
     filename = os.path.join(root_dir, class_filename)
     with open(filename, 'w') as f:
-        f.write('\n'.join(all_classes) + '\n')
+        f.write(out_str)
 
 
 def main():
     script_path = os.path.dirname(os.path.realpath(__file__))
     all_ast_files = get_all_ast_files(script_path)
     all_classes = sorted({c for f in all_ast_files for c in get_classes_in_file(f)})
-    write_class_file(script_path, all_classes)
+    write_class_file(script_path, all_classes, True)
+    # write_class_file(script_path, all_classes)
 
 
 if __name__ == '__main__':
